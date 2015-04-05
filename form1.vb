b2c504dc86485e402cc8ac3c201bd179b81b377c
PeoLeser <PeoLeser@users.noreply.github.com>
2015-04-05 13:30:54 +0800

    Update form1.vb

--- form1.vb
+++ form1.vb
@@ -8,32 +8,16 @@ Imports System.Net
 Imports System.Net.Sockets
 Imports System.Text
 Imports System.Text.RegularExpressions
-Public Class Form1
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
-        '获取内存
-        LabelTotalMemNum.Text = Int(My.Computer.Info.TotalPhysicalMemory / 1024 / 1024) & " M"
-        LabelAvailableMemNum.Text = Int(My.Computer.Info.AvailablePhysicalMemory / 1024 / 1024) & " M"
-        TextBoxAvailableMem.Text = Int(My.Computer.Info.AvailablePhysicalMemory / 1024 / 1024 * 0.9)
+Public Class Main
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles Me.Load
+
         '获取注册表
         If Registry.CurrentUser.OpenSubKey("SOFTWARE\LightMCLauncher", True) IsNot Nothing Then
             TextBoxUsername.Text = Registry.CurrentUser.OpenSubKey("SOFTWARE\LightMCLauncher", True).GetValue("Username")
             TextBoxParameter.Text = Registry.CurrentUser.OpenSubKey("SOFTWARE\LightMCLauncher", True).GetValue("Parameter")
         End If
-        '获取服务器信息
-        Dim ip As String
-        Dim ipHost As IPHostEntry = Dns.GetHostEntry("mc.ime.moe")
-        For Each ip1 As IPAddress In ipHost.AddressList
-            ip = ip1.ToString
-            Exit For
-        Next
-        Dim a As eMZi.Gaming.Minecraft.MinecraftServerInfo
-        Dim c As IPAddress
-        c = IPAddress.Parse(ip)
-        Dim b As New IPEndPoint(c, 25565)
-        a = eMZi.Gaming.Minecraft.MinecraftServerInfo.GetServerInformation(b)
-        LabelServerVersion.Text = "服务器版本:" & a.MinecraftVersion
-        LabelServerPlayerCount.Text = "当前在线人数:" & a.CurrentPlayerCount & "/" & a.MaxPlayerCount
 
         '释放更新文件
         Dim FileName2 As String = "MCUpdater.ini", FileName3 As String = "MCUpdater.exe"
@@ -150,6 +134,10 @@ Public Class Form1
         Application.Exit()
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SetReg()
+    End Sub
+
 #Region "设置MC启动命令行"
     Private Function GetMcPath()
         Dim dirAimPathTemp As New IO.DirectoryInfo(Environment.CurrentDirectory & "\.minecraft\versions\"), strAimPath As String, strForgePath As String, strDefaultPara As String
@@ -160,16 +148,15 @@ Public Class Form1
         Dim strTmpLib As String = Environment.CurrentDirectory & "\.minecraft\libraries\"
         '设置附加参数
         strDefaultPara = " -XX:-UseVMInterruptibleIO -XX:NewRatio=3 -XX:+UseStringCache -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseBiasedLocking -XX:PermSize=128m -XX:MaxPermSize=256m -XX:+CMSParallelRemarkEnabled -XX:MaxGCPauseMillis=50 -XX:+UseAdaptiveGCBoundary -XX:-UseGCOverheadLimit -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15 -XX:+UseAdaptiveSizePolicy -XX:+DisableExplicitGC -Xnoclassgc -oss4M -ss4M -XX:CMSInitiatingOccupancyFraction=60 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=1 -XX:SoftRefLRUPolicyMSPerMB=2048 -Xms800M -XX:ParallelGCThreads=" & System.Environment.ProcessorCount & " -Dfml.ignoreInvalidMinecraftCertificates=true -Dfml.ignorePatchDiscrepancies=true "
-        strMcPara = strDefaultPara & TextBoxParameter.Text & " -Djava.library.path=" & """" & ".minecraft\natives" & """" & " -cp "
+        strMcPara = strDefaultPara & TextBoxParameter.Text & " -Djava.library.path=" & Chr(34) & ".minecraft\natives" & Chr(34) & " -cp "
         '设置命令行
-        strShell = """" & GetJavaHome() & """" & " -Xmx" & TextBoxAvailableMem.Text & "M" & strMcPara & """" & GetLibrariesFiles(strLibPath) & Environment.CurrentDirectory & "\.minecraft\versions\" & strForgePath & "\" & strForgePath & ".jar" & """" & " net.minecraft.launchwrapper.Launch  --username " & TextBoxUsername.Text & " --version " & strForgePath & " --gameDir .minecraft\versions\" & strForgePath & " --assetsDir .minecraft\assets --assetIndex 1.7.10 --uuid ${auth_uuid} --accessToken ${auth_access_token} --userProperties {} --userType Legacy --tweakClass cpw.mods.fml.common.launcher.FMLTweaker"
+        strShell = Chr(34) & GetJavaHome() & Chr(34) & " -Xmx" & TextBoxAvailableMem.Text & "M" & strMcPara & Chr(34) & GetLibrariesFiles() & Environment.CurrentDirectory & "\.minecraft\versions\" & strForgePath & "\" & strForgePath & ".jar" & Chr(34) & " net.minecraft.launchwrapper.Launch  --username " & TextBoxUsername.Text & " --version " & strForgePath & " --gameDir .minecraft\versions\" & strForgePath & " --assetsDir .minecraft\assets --assetIndex 1.7.10 --uuid ${auth_uuid} --accessToken ${auth_access_token} --userProperties {} --userType Legacy --tweakClass cpw.mods.fml.common.launcher.FMLTweaker"
         Return strShell
     End Function
 #End Region
 
 #Region "设置注册表"
     Private Sub SetReg()
-        Dim regUsernameKey As RegistryKey
         If Registry.CurrentUser.OpenSubKey("SOFTWARE\LightMCLauncher", True) Is Nothing Then
             Registry.CurrentUser.CreateSubKey("Software\LightMCLauncher")
             Registry.CurrentUser.OpenSubKey("SOFTWARE\LightMCLauncher", True).SetValue("Username", TextBoxUsername.Text)
@@ -216,39 +203,83 @@ Public Class Form1
 #End Region
 
 #Region "获取Libraries文件列表"
-    Function GetLibrariesFiles(ByVal strLibPath As String)
-        Dim strLibFiles() As String, i As Integer, strMcLibraries As String
-        strLibFiles = IO.Directory.GetFiles(strLibPath, "*.jar", SearchOption.AllDirectories)
-        Do Until i = strLibFiles.Length
-            strMcLibraries = strMcLibraries & strLibFiles(i) & ";"
-            i += 1
-        Loop
-        Return strMcLibraries
-        Return strLibFiles.Length
+    Function GetLibrariesFiles()
+        'Function GetLibrariesFiles(ByVal strLibPath As String)
+        'Dim strLibFiles() As String, i As Integer, strMcLibraries As String
+        'strLibFiles = IO.Directory.GetFiles(strLibPath, "*.jar", SearchOption.AllDirectories)
+        'Do Until i = strLibFiles.Length
+        '    strMcLibraries = strMcLibraries & strLibFiles(i) & ";"
+        '    i += 1
+        'Loop
+        'Return strMcLibraries
+        Dim libraries2(100) As String
+        Dim mi As Integer = 0
+        Dim libraries1 As Integer = 0
+        Dim a As String
+        Dim json As String
+        Dim json1 As String()
+        json = File.ReadAllText(Directory.GetCurrentDirectory() + "\.minecraft\versions\" + "1.7.10-Forge10.13.2.1291" + "\" + "1.7.10-Forge10.13.2.1291" + ".json")
+        json1 = json.Split(New String() {Chr(34)}, StringSplitOptions.RemoveEmptyEntries)
+        For Each m As String In json1
+            If m = "name" Then
+                mi = 4
+            End If
+            If mi > 0 Then
+                mi = mi - 1
+            End If
+            If mi = 1 And m.Contains(".") And m.Contains(":") Then
+
+                Dim mlj As String
+                Dim mname As String
+                Dim mmh As Integer = InStr(m, ":")
+                mlj = Strings.Left(m, mmh - 1)
+                mlj = Directory.GetCurrentDirectory() + "\.minecraft\libraries\" + mlj.Replace(".", "\")
+                mname = Strings.Right(m, m.Length - mmh + 1)
+                mlj = mlj + mname.Replace(":", "\") + "\"
+                mname = Strings.Right(m, m.Length - mmh)
+                mname = mname.Replace(":", "-") + ".jar;"
+
+
+                libraries2(libraries1) = mlj.Replace(Directory.GetCurrentDirectory() + "\.minecraft\libraries\", "") + mname.Replace(";", "")
+
+                libraries1 = libraries1 + 1
+                a = a + mlj + mname
+            End If
+        Next
+        Return a
     End Function
 #End Region
-
-    Private Sub ButtonRefreshMem_Click(sender As Object, e As EventArgs) Handles ButtonRefreshMem.Click
-        LabelAvailableMemNum.Text = Int(My.Computer.Info.AvailablePhysicalMemory / 1024 / 1024) & " M"
-        TextBoxAvailableMem.Text = Int(My.Computer.Info.AvailablePhysicalMemory / 1024 / 1024 * 0.9)
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        SetReg()
-    End Sub
 
     Private Sub ButtonDefaultParameter_Click(sender As Object, e As EventArgs) Handles ButtonDefaultParameter.Click
         MessageBox.Show("-XX:-UseVMInterruptibleIO -XX:NewRatio=3 -XX:+UseStringCache -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseBiasedLocking -XX:PermSize=128m -XX:MaxPermSize=256m -XX:+CMSParallelRemarkEnabled -XX:MaxGCPauseMillis=50 -XX:+UseAdaptiveGCBoundary -XX:-UseGCOverheadLimit -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15 -XX:+UseAdaptiveSizePolicy -XX:+DisableExplicitGC -Xnoclassgc -oss4M -ss4M -XX:CMSInitiatingOccupancyFraction=60 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=1 -XX:SoftRefLRUPolicyMSPerMB=2048 -Xms800M -XX:ParallelGCThreads=" & System.Environment.ProcessorCount & " -Dfml.ignoreInvalidMinecraftCertificates=true -Dfml.ignorePatchDiscrepancies=true ")
     End Sub
 
+    Private Sub TimerRefreshInfo_Tick(sender As Object, e As EventArgs) Handles TimerRefreshInfo.Tick
+        '获取内存
+        LabelTotalMemNum.Text = Int(My.Computer.Info.TotalPhysicalMemory / 1024 / 1024) & " M"
+        LabelAvailableMemNum.Text = Int(My.Computer.Info.AvailablePhysicalMemory / 1024 / 1024) & " M"
+        TextBoxAvailableMem.Text = Int(My.Computer.Info.AvailablePhysicalMemory / 1024 / 1024 * 0.9)
+        '获取服务器信息
+        Dim ip As String
+        Dim ipHost As IPHostEntry = Dns.GetHostEntry("mc.ime.moe")
+        For Each ip1 As IPAddress In ipHost.AddressList
+            ip = ip1.ToString
+            Exit For
+        Next
+        Dim a As eMZi.Gaming.Minecraft.MinecraftServerInfo
+        Dim c As IPAddress
+        c = IPAddress.Parse(ip)
+        Dim b As New IPEndPoint(c, 25565)
+        a = eMZi.Gaming.Minecraft.MinecraftServerInfo.GetServerInformation(b)
+        LabelServerVersion.Text = "服务器版本:" & a.MinecraftVersion
+        LabelServerPlayerCount.Text = "当前在线人数:" & a.CurrentPlayerCount & "/" & a.MaxPlayerCount
+    End Sub
 
 End Class
 
 Namespace eMZi.Gaming.Minecraft
-    ''' <summary>
-    ''' thanks to "mcmny"
-    ''' </summary>
-    ''' <remarks></remarks>
+
+    ' thanks to mcmny@mcbbs & eMZi@github
 
     Public NotInheritable Class MinecraftServerInfo
 
